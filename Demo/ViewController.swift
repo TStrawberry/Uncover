96393d30511cf15dc0c211759e1ab5403c31efdd
TStrawberry <me@tstrawberry.com>
2018-11-27 16:17:38 +0800

    Initialize project

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -11,11 +11,37 @@ import Uncover
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initialUncover()
+        
+        
+        
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
 }
+
+
+extension ViewController : FocusItemUncoverProtocol {
+
+    func uncoverItem(for responder: UIResponder) -> FocusItem {
+        return responder as! UIView
+    }
+    
+    func move(_ item: FocusItem, by suggestion: Movement) {
+        bottomConstraint.constant = bottomConstraint.constant - suggestion.numerical
+        UIView.animate(withDuration: 0.25) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+}
+
 
--- Uncover/Uncover.swift
+++ Uncover/Uncover.swift
@@ -8,22 +8,36 @@
 
 import UIKit
 
-public protocol FocusItem {
+public enum Movement {
+    case up(CGFloat)
+    case down(CGFloat)
+    case none
+    
+    public var numerical: CGFloat {
+        switch self {
+        case let .up(value): return -value
+        case let .down(value): return value
+        default: return 0
+        }
+    }
+    
+}
+
+public protocol FocusItem : UICoordinateSpace {
     var frame: CGRect { get }
 }
 
-extension UIResponder : FocusItem {
-    public var frame: CGRect { return accessibilityFrame }
+extension UIView : FocusItem { }
+
+public protocol FocusItemUncoverProtocol : NSObjectProtocol {
+    
+    func uncoverItem(for responder: UIResponder) -> FocusItem
+    
+    func move(_ item: FocusItem, by suggestion: Movement)
+    
 }
 
-
-protocol ViewUncoverProtocol : NSObjectProtocol {
-    
-    func uncoverView(for responder: UIResponder) -> FocusItem
-    
-}
-
-extension ViewUncoverProtocol {
+public extension FocusItemUncoverProtocol {
     
     func initialUncover() {
         NotificationCenter.default
@@ -43,21 +57,53 @@ extension ViewUncoverProtocol {
     
 }
 
-extension ViewUncoverProtocol {
+struct KeyBoardUserInfo {
     
-    func keyBoardFrameWillChange(userInfo: [AnyHashable: Any]?) {
-        
-        guard let `userInfo` = userInfo else { return }
-        
-        let keyboardEndFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
-        
+    /*
+     [AnyHashable("UIKeyboardCenterBeginUserInfoKey"): NSPoint: {207, 871.5},
+     AnyHashable("UIKeyboardBoundsUserInfoKey"): NSRect: {{0, 0}, {414, 271}},
+     AnyHashable("UIKeyboardFrameEndUserInfoKey"): NSRect: {{0, 465}, {414, 271}},
+     AnyHashable("UIKeyboardCenterEndUserInfoKey"): NSPoint: {207, 600.5},
+     AnyHashable("UIKeyboardIsLocalUserInfoKey"): 1,
+     AnyHashable("UIKeyboardAnimationDurationUserInfoKey"): 0.25,
+     AnyHashable("UIKeyboardAnimationCurveUserInfoKey"): 7,
+     AnyHashable("UIKeyboardFrameBeginUserInfoKey"): NSRect: {{0, 736}, {414, 271}}]
+     */
+    
+    var endFrame: CGRect { return userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect }
+    
+    private let userInfo: [AnyHashable: Any]
+    
+    init(_ userInfo: [AnyHashable: Any]) {
+        self.userInfo = userInfo
     }
-    
+
 }
 
 
-
-
+extension FocusItemUncoverProtocol {
+    
+    func keyBoardFrameWillChange(userInfo: [AnyHashable: Any]?) {
+        
+        guard let `userInfo` = userInfo, let currentResponder = UIResponder.currentFirstResponder() else { return }
+        
+        let keyBoardUserInfo = KeyBoardUserInfo(userInfo)
+        let endFrame = keyBoardUserInfo.endFrame
+        
+        let focusItem = uncoverItem(for: currentResponder)
+        let screenOrientedFrame = focusItem.convert(focusItem.frame, to: UIScreen.main.fixedCoordinateSpace)
+        
+        print(UIScreen.main.bounds)
+        print(endFrame)
+        print(screenOrientedFrame)
+        
+        let distance = screenOrientedFrame.maxY - endFrame.minY
+        let movement: Movement = distance > 0 ? .up(distance) : .none
+        
+        move(focusItem, by: movement)
+    }
+    
+}
 
 
 
